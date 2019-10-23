6b9a6f184a0bd08516c835cc95c0fd38dd388b15
Robinson Regalado <rregalado17@gmail.com>
2019-10-23 19:48:43 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,5 +1,6 @@
 class Helpers
   
+<<<<<<< HEAD
   def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
   end
@@ -7,4 +8,13 @@ class Helpers
   def self.is_logged_in?(session)
     !!session[:user_id]
   end
+=======
+  #def self.current_user(session)
+   # @user = User.find_by_id(session[:user_id])
+  #end
+  
+  #def self.is_logged_in?(session)
+  #  !!session[:user_id]
+  #end
+>>>>>>> 065e7290ee7e1da5cf58aace74565ec3b1b89364
 end
